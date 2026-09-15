f278819780769a3385f23ac569ba49ad19172688
isaacnwc <isaac.canario@outlook.com>
2026-09-15 17:10:30 -0300

    Add banner grabing

--- scanner/core.py
+++ scanner/core.py
@@ -1,13 +1,20 @@
 import socket
 
-def get_available_ports(address, min_port, max_port):
+def scan_ports(address, min_port, max_port):
     open_ports= []
     for port in range(min_port, max_port + 1):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(0.5)
         connection_status = s.connect_ex((address, port))
+
         if connection_status== 0:
-            open_ports.append(port)
+            try:
+                banner= s.recv(1024).decode(errors="ignore").strip()
+            except socket.timeout:
+                banner = None
+
+            open_ports.append((port,banner))
+
         s.close()
     return open_ports
 
