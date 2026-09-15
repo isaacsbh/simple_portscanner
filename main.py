60362446a2188ffd774b55770273a3dbebd016e6
isaacnwc <isaac.canario@outlook.com>
2026-09-15 17:08:53 -0300

    Fix function name
    get_avaliable_ports -> scan_ports

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-from scanner.core import get_available_ports
+from scanner.core import scan_ports
 
 def main (argv): 
     address = sys.argv[1]
     min_port = int(sys.argv[2])
     max_port = int(sys.argv[3])
-    ports = get_available_ports(address, min_port, max_port)
+    ports = scan_ports(address, min_port, max_port)
     for p in ports: 
         print(p)
 
